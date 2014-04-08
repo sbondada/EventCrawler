25a10704cc3b9d860619bbc0caadde2b9fc3fbf1
sbondada <sbondada@buffalo.edu>
2014-04-07 21:14:42 -0400

    added the code to output the links and scores in a jason file

--- eventcrawler/eventcrawler/pipelines.py
+++ eventcrawler/eventcrawler/pipelines.py
@@ -2,7 +2,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 class EventcrawlerPipeline(object):
+    def __init__(self):
+        self.file=open('outlinks.jl','wb')
+
     def process_item(self, item, spider):
+        line=json.dumps(dict(item))+"\n"
+        self.file.write(line)
         return item
--- eventcrawler/eventcrawler/settings.py
+++ eventcrawler/eventcrawler/settings.py
@@ -10,6 +10,12 @@ BOT_NAME = 'eventcrawler'
 
 SPIDER_MODULES = ['eventcrawler.spiders']
 NEWSPIDER_MODULE = 'eventcrawler.spiders'
+DEPTH_LIMIT = 5
+DEPTH_PRIORITY = 0
+DEPTH_STATS = True
+ITEM_PIPELINES = {
+                    'eventcrawler.pipelines.EventcrawlerPipeline': 300,
+                 }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'eventcrawler (+http://www.yourdomain.com)'
